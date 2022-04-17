0e61f1839f8bbda3fa56dbf6e5489e38c3848625
Oleksii Lialka <lialka.dev@protonmail.ch>
2022-04-17 21:48:12 +0200

    Added debian setup script

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # debian-setup
-Debian post-install setup script 
+Debian post-install setup script
+
+https://github.com/lialkaas/debian-setup
+
+* runs _remote_ post-install setup
+
+https://gitlab.com/defcronyke/debian-post-install
+
+* runs _custom_ post-install setup
+* installs VScode.
+
+## Run
+```shell
+su - -c 'apt update && apt install -y wget' && \
+bash <(wget -qO - https://bit.ly/3Om4LkL)
+```
+
---
+++ run-debian-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+remote_setup()
+{
+    su - -c 'apt update && apt install -y wget && \
+    bash <(wget -qO - https://bit.ly/debian-post-install)'
+    echo "Complete remote post-install setup."
+}
+
+custom_setup()
+{
+    su - -c 'apt remove -y gnome-games xterm rhythmbox evolution && \
+    apt autoremove -y; \
+    apt install -y build-essential dkms linux-headers-$(uname -r) && \
+    apt install -y curl rsync cmus vlc thunderbird calibre htop; \
+    apt install -y  klavaro vim zsh gnome-boxes git'
+    echo "Complete custom post-install setup."
+}
+
+install_vscode()
+{
+    su - -c 'sudo apt-get install -y wget gpg && \
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg && \
+    install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/ && \
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list && \
+    rm -f packages.microsoft.gpg && \
+    apt install -y apt-transport-https && \
+    apt update -y && \
+    apt install -y code'
+    echo "Installed VScode."
+}
+
+# Main script
+remote_setup
+custom_setup
+install_vscode
+echo "Complete Debian setup. Logout to enable sudo support."
